6abdd2bf231d947020863a58ba3496272da751c0
Bastien <neitsabast@gmail.com>
2024-05-29 19:17:00 +0200

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Docker NGINX with Modules
+
+This project provides a set of scripts that simplify the process of creating a Docker image of NGINX with desired modules.
+
+This project uses Dockerfiles provided by NGINX maintainers to easily generate NGINX images with modules. It supports both Debian and Alpine base images, as well as both regular NGINX and Unprivileged images.
+
+## Usage
+
+To use the script, you need to provide three parameters when calling the bash script:
+
+1. `MODULES_TO_ADD_LIST`: A list of modules you want to include, separated by spaces. These modules should be available on pkg-oss.
+2. `BASE_NGINX_IMAGE`: The NGINX base image to which you want to add the modules. Defaults to the mainline image.
+3. `IMAGE_NAME_GENERATED`: The name of the generated image.
+
+Here's an example command:
+
+```bash
+./nginx-alpine.sh
+```
+
+## More information
+
+- The GitHub project for the NGINX Docker image : https://github.com/nginxinc/docker-nginx
+    - The modules part : https://github.com/nginxinc/docker-nginx/tree/master/modules
+
+- I have written an article about how to create a Docker Image with modules (in French 🇫🇷) : https://bastienbyra.fr/blog/comment-modifier-ou-retirer-le-header-server-de-nginx/
---
+++ nginx-unprivileged-alpine.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+MODULES_TO_ADD_LIST=""
+IMAGE_NAME_GENERATED=""
+BASE_NGINX_IMAGE="nginxinc/nginx-unprivileged:mainline"
+
+# Get the current folder and check if .env file exists before sourcing it
+dir="$(dirname "$0")"
+if [ -f "$dir/.env" ]; then
+    source "$dir/.env"
+fi
+
+# Check if the environnement variables are set
+if [ -z "${MODULES_TO_ADD_LIST}" ]; then
+    echo "MODULES_TO_ADD_LIST is not set"
+    exit 1
+else
+    MODULES_TO_ADD_LIST=${MODULES_TO_ADD_LIST}
+fi
+
+
+if [ -z "${IMAGE_NAME_GENERATED}" ]; then
+    echo "IMAGE_NAME_GENERATED is not set"
+    exit 1
+else
+    IMAGE_NAME_GENERATED=${IMAGE_NAME_GENERATED}
+fi
+
+# Copy the Dockerfile that will get the module on the host
+curl https://raw.githubusercontent.com/nginxinc/docker-nginx/master/modules/Dockerfile.alpine > Dockerfile.build.temp
+
+# Change to user root to install the dependencies
+sed -i '/SHELL \["\/bin\/bash", "-exo", "pipefail", "-c"\]/N; /SHELL \["\/bin\/bash", "-exo", "pipefail", "-c"\]/a USER root' Dockerfile.build.temp
+sed -i '/^FROM ${NGINX_FROM_IMAGE}$/a USER root' Dockerfile.build.temp
+
+# Go back to unprivileged user
+echo "USER nginx" >> Dockerfile.build.temp
+
+# Run the dockerfile to generate the clean image with modules
+export DOCKER_BUILDKIT=1
+docker build -f Dockerfile.build.temp \
+    --build-arg ENABLED_MODULES="$MODULES_TO_ADD_LIST" \
+    --build-arg NGINX_FROM_IMAGE="$BASE_NGINX_IMAGE" \
+    -t "$IMAGE_NAME_GENERATED" \
+    .
+
+# Delete the Dockerfile
+rm Dockerfile.build.temp
